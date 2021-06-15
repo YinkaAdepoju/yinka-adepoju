2593500080f0af39df31307c32cd53d508f7cc21
Yinka <yinka.social@icloud.com>
2021-06-15 23:27:18 +0200

    commit8

--- gh-pages.js
+++ gh-pages.js
@@ -1,13 +1,13 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish(
-    'Users/yinka/Documents/GitHub/yinka-adepoju/public', // path to public directory
+    'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/YinkaAdepoju/yinka-adepoju', // Update to point to your repository  
+        repo: 'https://github.com/YinkaAdepoju/yinka-adepoju.git', // Update to point to your repository  
         user: {
             name: 'YinkaAdepoju', // update to use your name
-            email: 'yinka.social@icl' // Update to use your email
+            email: 'yinka.social@icloud.com' // Update to use your email
         }
     },
     () => {
